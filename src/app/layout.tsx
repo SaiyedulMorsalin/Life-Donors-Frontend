ac2a6c0502eb19e96d934e6fed035d0073c974f4
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-09-01 00:35:33 +0600

    new deploy

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import NavBar from "@/components/NavBar";
+import { type Metadata } from "next";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "LifeDonors",
+  description: "Give red love save red lives",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${inter.className}`}>
+      <head>
+        <title>LifeDonors</title>
+      </head>
+      <body className="relative">
+        <NavBar />
+        {children}
+        <Footer />
+        <Toaster />
+      </body>
+    </html>
+  );
+}

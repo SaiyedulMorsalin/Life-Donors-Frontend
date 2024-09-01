2e33f3ab7417d7a6b3caeedb2dcecb2f9d2d80e1
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-09-02 03:11:04 +0600

    fix bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-destructive py-4 text-center text-sm text-white/80 md:text-base">
-      Copyright © LifeDonors 2015 - Present | SaiyedulM.
+      Copyright © LifeDonors 2015 - Present | Made with TOPU
     </footer>
   );
 }
